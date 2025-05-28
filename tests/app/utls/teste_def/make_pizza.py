b4898c135fd517a0b71f0ba070708a701665a149
Rafael Sanchez <88865750+RafaellSanchez@users.noreply.github.com>
2025-05-28 13:35:19 +0000

    test def

---
+++ tests/app/utls/teste_def/make_pizza.py
@@ -0,0 +1,11 @@
+# import pizza
+# pizza.make_pizza(16, 'peperonni')
+# pizza.make_pizza(6, 'mussarela')
+
+# from pizza import make_pizza
+
+# make_pizza(12, 'portuguesa')
+
+from pizza import make_pizza as mp
+
+mp(12, 'calabresa')
